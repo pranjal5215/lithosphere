2088cbf8838cf6aa409d7ea5bee7300e0f57b7b5
harshad <harshad.patil@ibibogroup.com>
2015-02-23 16:57:56 +0530

    added mysql pooling file

---
+++ mysql.go
@@ -0,0 +1,71 @@
+package lithosphere
+
+import (
+	"database/sql"
+	"errors"
+	log "github.com/Sirupsen/logrus"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/goibibo/gopool"
+	"github.com/goibibo/t-settings"
+)
+
+const (
+	MIN_CONNS = 10
+	MAX_CONNS = 100
+)
+
+type MySqlPool struct {
+	*pool.Pool
+}
+
+var mySqlPoolMap map[string]MySqlPool
+
+func init() {
+	mySqlPoolMap = make(map[string]MySqlPool)
+}
+
+func newMySqlPool(connStr string) MySqlPool {
+	create := func() interface{} {
+		db, err := sql.Open("mysql", connStr)
+		if err != nil {
+			return nil
+		}
+		return db
+	}
+	p := pool.CreatePool(MIN_CONNS, MAX_CONNS,
+		create,
+		func(i interface{}) {
+			db := i.(sql.DB)
+			err := db.Close()
+			if err != nil {
+				log.Error("mysql db closing failed")
+			}
+		})
+	return MySqlPool{p}
+}
+
+func GetMySqlPool(vertical string) MySqlPool {
+	if pool, ok := mySqlPoolMap[vertical]; ok {
+		return pool
+	} else {
+		configs := settings.GetConfigsFor("mysql", vertical)
+		connectionString := settings.ConstructMysqlPath(configs)
+		mySqlPoolMap[vertical] = newMySqlPool(connectionString)
+		return mySqlPoolMap[vertical]
+	}
+}
+
+func (m MySqlPool) Get() (sql.DB, error) {
+	var db sql.DB
+	var ok bool
+	d := m.Acquire()
+	db, ok = d.(sql.DB)
+	if ok {
+		return db, nil
+	}
+	return db, errors.New("db loading failed")
+}
+
+func (m MySqlPool) Return(db sql.DB) {
+	m.Release(db)
+}
