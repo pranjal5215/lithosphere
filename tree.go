f6a33973a96128bb21257684a13795e778bb8549
harshad <harshad.patil@ibibogroup.com>
2015-04-09 19:50:47 +0530

    added interface to leaves function

--- tree.go
+++ tree.go
@@ -1,16 +1,17 @@
 package lithosphere
 
 type Tree struct {
-	M MergeFunc
-	L []LeafFunc
+	M    MergeFunc
+	L    []LeafFunc
+	Linp []interface{}
 }
 
-type LeafFunc func()
+type LeafFunc func(inp interface{})
 type MergeFunc func(size int)
 
 func (t *Tree) DoTree(size int) {
-	for _, f := range t.L {
-		go f() //all f's should push to channel
+	for k, f := range t.L {
+		go f(t.Linp[k]) //all f's should push to channel
 	}
 	t.M(size) //M should listen to channel and process
 }
